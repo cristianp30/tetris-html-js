c1a2702dfb23a4a43e30da987852e85c70d7f42b
cristian_parra <pacc2430@gmail.com>
2018-12-16 07:42:14 -0500

    se agrega la funcion update

--- js/tetris.js
+++ js/tetris.js
@@ -1,7 +1,18 @@
+let lastTime = 0;
+
+
 const canvas = document.getElementById("tetris");
 const context = canvas.getContext("2d");
 
 context.scale(20,20);
 
-context.fillStyle = "#000";
-context.fillRect(0, 0, canvas.width, canvas.height);
+
+
+function update (time = 0){
+    const deltaTime = time - lastTime;
+    lastTime = time;
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+    requestAnimationFrame(update);
+}
+    update();
