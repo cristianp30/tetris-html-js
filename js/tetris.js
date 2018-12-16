4b23c9100c69f400b2e272f70ad189d1a9e2d919
cristian_parra <pacc2430@gmail.com>
2018-12-16 08:29:34 -0500

    se agrega la funcion draw

--- js/tetris.js
+++ js/tetris.js
@@ -6,13 +6,15 @@ const context = canvas.getContext("2d");
 
 context.scale(20,20);
 
-
+funtion draw(){
+    context.fillStyle = "#000";
+    context.fillRect(0, 0, canvas.width, canvas.height);
+}
 
 function update (time = 0){
     const deltaTime = time - lastTime;
     lastTime = time;
-    context.fillStyle = "#000";
-    context.fillRect(0, 0, canvas.width, canvas.height);
+    
     requestAnimationFrame(update);
 }
     update();
