98de2594197368144a70fb48947180663a493536
cristian_parra <pacc2430@gmail.com>
2018-12-16 09:16:41 -0500

    se creo la funcion drawMatriz

--- js/tetris.js
+++ js/tetris.js
@@ -4,7 +4,14 @@ let lastTime = 0;
 const canvas = document.getElementById("tetris");
 const context = canvas.getContext("2d");
 const grid = createMatriz(10,20);
-
+const player = {
+    pos: {x: 0, y: 0},
+    matriz: [
+        [0,0,0],
+        [1,1,1],
+        [0,1,0]
+    ]
+};
 context.scale(20,20);
 
 function createMatriz(width, height){
@@ -17,16 +24,31 @@ function createMatriz(width, height){
     return matriz;
 }
 
+function drawMatriz(matriz, offset) {
+    matriz.forEach((row, y) => {
+        row.forEach((value, x)=>{
+            if(value!==0){
+                context.fillStyle = "red";
+                context.fiilRect(x + offset.x; y + offset.y, 1,1);
+            }
+        })
+        
+    });
+    
+}
+
 function draw(){
     context.fillStyle = "#000";
     context.fillRect(0, 0, canvas.width, canvas.height);
+    drawMatriz(grid, {x:0, y:0});
 }
 
 function update (time = 0){
     const deltaTime = time - lastTime;
-    lastTime = time;
     
+    lastTime = time;
+    draw();
     requestAnimationFrame(update);
 }
-   draw();
-update();
+  
+    update();
